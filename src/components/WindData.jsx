cadfd720152d52bece0095a48ac8d4933351b494
MikaelRothig <mrrothig@gmail.com>
2025-02-08 00:07:45 +0200

    Styling updates

--- src/components/WindData.jsx
+++ src/components/WindData.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 export default function GetWindData(){
     const [showEntries, setShowEntries] = useState()
-    // const apiUrl = 'http://localhost:4321/testData-15min.json';
-    const apiUrl = 'https://weather-station-dashboard-xi.vercel.app/testData-15min.json';
+    const apiUrl = 'http://localhost:4321/testData-15min.json';
 
     function pullJson() {
+        let index = 0;
         fetch(apiUrl)
             .then(response => response.json())
             .then(responseData => {
@@ -13,16 +13,20 @@ export default function GetWindData(){
                     let bgLow = getBackgroundColor(entry.low)
                     let bgAvg = getBackgroundColor(entry.avg)
                     let bgHigh = getBackgroundColor(entry.high)
+                    index++
 
                     return (
-                        <div>
+                        <div class="space-y-2">
                             <div className="grid grid-cols-12 gap-x-2">
                                 <span className="flex col-span-3 items-center justify-center px-4 py-2 font-bold text-sm">{entry.time}</span>
-                                <span className={`flex col-span-2 items-center justify-center px-4 py-2 rounded font-bold text-sm ${bgLow}`}>{Math.round(entry.low)}</span>
-                                <span className={`flex col-span-2 items-center justify-center px-4 py-2 rounded font-bold text-sm ${bgAvg}`}>{Math.round(entry.avg)}</span>
-                                <span className={`flex col-span-2 items-center justify-center px-4 py-2 rounded font-bold text-sm ${bgHigh}`}>{Math.round(entry.high)}</span>
-                                <span className="flex col-span-3 items-center justify-center px-4 py-2 bg-zinc-900 rounded font-bold text-sm">{entry.dir}</span>
+                                <span className={`flex col-span-2 items-center justify-center px-4 py-2 rounded-md font-bold text-sm ${bgLow}`}>{Math.round(entry.low)}</span>
+                                <span className={`flex col-span-2 items-center justify-center px-4 py-2 rounded-md font-bold text-sm ${bgAvg}`}>{Math.round(entry.avg)}</span>
+                                <span className={`flex col-span-2 items-center justify-center px-4 py-2 rounded-md font-bold text-sm ${bgHigh}`}>{Math.round(entry.high)}</span>
+                                <span className="flex col-span-3 items-center justify-center px-4 py-2 bg-zinc-900 rounded-md font-bold text-sm">{entry.dir}</span>
                             </div>
+                            {index !== responseData.length && (
+                                <div className="border border-t-zinc-900" />
+                            )}
                         </div>
                     )
                 }))
