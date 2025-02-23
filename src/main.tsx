78aab616f06e64a43b631feeddc8dc19a76e34a3
MikaelRothig <mrrothig@gmail.com>
2025-02-23 14:14:51 +0200

    Minor refactor

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {Analytics} from "@vercel/analytics/vue";
+import {Analytics} from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Analytics/>
+      <App />
+      <Analytics/>
   </StrictMode>,
 )
