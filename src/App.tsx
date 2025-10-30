1634d54d98e55235cb84a16318679e400ae79f42
MikaelRothig <mrrothig@gmail.com>
2025-10-30 22:20:19 +0100

    Removed header reference

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,6 @@ function App() {
     return (
         <div className="max-w-[1536px] mx-auto min-h-screen flex flex-col">
             <div className="px-4 md:px-8 space-y-8 flex-grow">
-                <Header />
-
                 <div className="flex flex-col gap-8">
                     <div className="grid grid-cols-4 xl:grid-cols-12 gap-8 xl:min-h-56">
                         <div className="col-span-2 bg-zinc-900 rounded-md h-40 xl:mt-11 xl:h-48">
